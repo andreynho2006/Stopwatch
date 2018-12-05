c852823d1fc3fe759fa060822ed2647e790f2b7b
andreynho2006 <andreynho2006@gmail.com>
2018-12-05 21:29:20 +0200

    add a Handler to schedule time

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -2,7 +2,10 @@ package com.hfad.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+import java.util.Locale;
 
 public class StopwatchActivity extends Activity {
 
@@ -13,8 +16,8 @@ public class StopwatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
-
 
     public void onClickStart(View view) {         // get's called when start button is clicked
         running = true;                           // start the stopwatch
@@ -27,5 +30,25 @@ public class StopwatchActivity extends Activity {
     public void onClickReset(View view) {         // get's called when stop button is clicked
         running = false;                          // stop the stopwatch
         seconds = 0;                              // reset to 0 the number of seconds
+    }
+
+    //Sets the number of seconds on the timer
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler - new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = ( seconds % 3600 ) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs );
+                timeView.setText(time);
+                if(running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
