cc56a8b9a7500c9d99b88bb1b814f24a6fcd2c8a
andreynho2006 <andreynho2006@gmail.com>
2018-12-05 22:31:24 +0200

    override the onSaveInstanceState to save data when activity is destroyed

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -16,7 +16,18 @@ public class StopwatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        if(savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
+
         runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        saveInstanceState.putInt("seconds", seconds);
+        saveInstanceState.putBoolean("running", running);
     }
 
     public void onClickStart(View view) {         // get's called when start button is clicked
@@ -35,16 +46,16 @@ public class StopwatchActivity extends Activity {
     //Sets the number of seconds on the timer
     private void runTimer() {
         final TextView timeView = (TextView)findViewById(R.id.time_view);
-        final Handler handler - new Handler();
+        final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours = seconds / 3600;
-                int minutes = ( seconds % 3600 ) / 60;
-                int secs = seconds % 60;
-                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs );
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
                 timeView.setText(time);
-                if(running) {
+                if (running) {
                     seconds++;
                 }
                 handler.postDelayed(this, 1000);
